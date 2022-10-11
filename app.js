fdfcd533222b43a4113326c65cb1f201c7698305
mvrk <oscarmenacho3@gmail.com>
2022-10-11 01:47:23 -0500

    Iniciando proyecto - RESTServer

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ', process.env.PORT)
+});
