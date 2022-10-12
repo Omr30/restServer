72fa6075ae66aaaa5e3c8502b83dd796553b1216
mvrk <oscarmenacho3@gmail.com>
2022-10-12 16:52:00 -0500

    Mongoose - Conectarnos a la base de datos

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 
 class Server {
@@ -9,6 +10,9 @@ class Server {
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
+        // Conectar a base de datos
+        this.conectarDB();
+
         // Middlewares
 
         this.middlewares();
@@ -16,6 +20,10 @@ class Server {
         // Rutas de mi app
 
         this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection()
     }
 
     middlewares() {
