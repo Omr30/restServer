0e4cdd7846cd1df3b7195acd1cb19ddc6fb7bc09
mvrk <oscarmenacho3@gmail.com>
2022-12-14 18:37:42 -0500

    Cambiar visualmente _id por uid en Mongoose

--- models/usuario.js
+++ models/usuario.js
@@ -41,7 +41,8 @@ const UsuarioSchema = Schema({
 });
 
 UsuarioSchema.methods.toJSON = function() {
-    const { __v, password, ...usuario } = this.toObject();
+    const { __v, password, _id, ...usuario } = this.toObject();
+    usuario.id = _id;
     return usuario
 }
 
