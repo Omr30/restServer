4b5d8f7f30b6c53fa9d19be7f6605e49248150b9
mvrk <oscarmenacho3@gmail.com>
2022-12-17 08:14:17 -0500

    Validar la extension

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -9,16 +9,29 @@ const cargarArchivo = async(req, res = response) => {
     }
 
     const {archivo} = req.files;
+    const nombreCortado = archivo.name.split('.')
+    const extension = nombreCortado[nombreCortado.length - 1]
 
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name)
-
-    archivo.mv(uploadPath, (err) => {
-            if (err) {
-                return res.status(500).send(err);
-        }
-
-            res.json({msg:'El archivo se subio a ' + uploadPath});
+    // Validar la extension
+    const  extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
+    if(!extensionesValidas.includes(extension)){
+        res.status(400).json({
+            msg:`La extension ${extension} no es permitida, ${extensionesValidas}`
         })
+    }
+
+    res.json({extension});
+
+
+    // const uploadPath = path.join(__dirname, '../uploads/', archivo.name)
+
+    // archivo.mv(uploadPath, (err) => {
+    //         if (err) {
+    //             return res.status(500).send(err);
+    //     }
+
+    //         res.json({msg:'El archivo se subio a ' + uploadPath});
+    //     })
 
 }
 
