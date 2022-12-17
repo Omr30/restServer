e2e4d7caa3ea7d6645ba5689f4ef2034eb300810
mvrk <oscarmenacho3@gmail.com>
2022-12-17 06:05:33 -0500

    Buscar por otros argumentos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -15,10 +15,20 @@ const buscarUsuarios = async(termino = '', res = response) => {
 
     if(esMongoId){
         const usuario = await Usuario.findById(termino);
-        res.json({
+        return res.json({
             results: (usuario) ? [usuario] : []
         })
     }
+
+    const regex = new RegExp( termino, 'i' )
+    const usuarios = await Usuario.find({
+        $or: [{nombre: regex}, {correo: regex}],
+        $and: [{estado: true}]
+    })
+
+    res.json({
+        results: usuarios
+    })
 
 }
 
