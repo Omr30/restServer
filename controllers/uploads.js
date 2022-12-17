d537dbf25ea62e490789f0e0bc203b7de424c288
mvrk <oscarmenacho3@gmail.com>
2022-12-17 16:35:50 -0500

    Borrar archivos del servidor usando fs y path

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const fs = require('fs')
+
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
 const { Usuario, Producto } = require("../models");
@@ -38,6 +41,15 @@ const actualizarImg = async(req, res = response) => {
     
         default:
             return res.status(500).json({msg: 'Se me olvido valdiar esto'})
+    }
+
+    // Limpiar imagenes previas
+    if(modelo.img){
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img)
+        if(fs.existsSync(pathImagen)) {
+            fs.unlinkSync(pathImagen)
+        }
     }
 
     const nombre = await subirArchivo(req.files, undefined, coleccion)
