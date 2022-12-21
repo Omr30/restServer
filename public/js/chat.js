f8137f543bcfb87f829b89671806471213f1660f
mvrk <oscarmenacho3@gmail.com>
2022-12-20 21:49:03 -0500

    Validar Token - Frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,39 @@
 
 
-const socket = io();
+const url = ( window.location.hostname.includes('localhost') )
+? 'http://localhost:8080/api/auth/'
+: 'https://restserver-production-1aab.up.railway.app/api/auth/';
+
+let usuario = null;
+let socket = null;
+
+const validarJWT = async() => {
+    const token = localStorage.getItem('token') || ''
+
+    if(token.length <= 10 ){
+        window.location = 'index.html'
+        throw new Error('No hay token en el servidor')
+    }
+
+    const resp = await fetch(url, {
+        headers: {'x-token': token}
+    })
+
+    const {usuario: userDB, token: tokenDB} = await resp.json()
+    localStorage.setItem('token', tokenDB)
+    usuario = userDB;
+
+}
+
+const main = async() => {
+
+    // Validar JWT
+    await validarJWT()
+
+
+}
+
+main();
+
+// const socket = io();
+
